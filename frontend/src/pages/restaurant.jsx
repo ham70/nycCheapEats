9b6fc469a915632693a71921138c84a88fa65895
Matthew Ham <hammatthew70@gmail.com>
2024-06-26 22:55:58 -0400

    trying to flesh out the page however the icons and social media links need more work

--- frontend/src/pages/restaurant.jsx
+++ frontend/src/pages/restaurant.jsx
@@ -3,6 +3,18 @@ import RestaurantDataService from '../services/restaurant.js'
 import { useParams } from 'react-router-dom';
 import SearchBar from '../components/searchbar.jsx';
 
+//social media icons
+import { FaMeta } from "react-icons/fa6";
+import { FaXTwitter } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa";
+
+//restaurant website icon
+import { CgWebsite } from "react-icons/cg";
+
+//icons array
+const icons = {CgWebsite, FaInstagram, FaXTwitter, FaMeta};
+
+
 const Restaurant = () => {
     //getting restuarant id and initializing inital state
     const { id } = useParams()
@@ -20,6 +32,8 @@ const Restaurant = () => {
 
     const [otherMediaImgs, setOtherMediaImgs] = useState([])
 
+    const [restaurantLinks, setLinks] = useState([])
+
     //
     const getRstaurant = id => {
         RestaurantDataService.get(id)
@@ -28,6 +42,7 @@ const Restaurant = () => {
                 setRestaurant(response.data.restaurant)
                 setImage(response.data.streetViewImg)
                 setOtherMediaImgs(response.data.otherMediaImgs)
+                setLinks(response.data.restaurant.links)
             })
             .catch(error => {
                 console.log(error)
@@ -49,13 +64,21 @@ const Restaurant = () => {
                         <h3 className='restaurant-title'>{restaurant.name}</h3>
                         <img src={`data:image/png;base64,${streetViewImg}`} className='main-image'/>
                         
-                        <p>{restaurant.address.building} {restaurant.address.street}, {restaurant.address.zipcode}</p>
-                        <p>{restaurant.description}</p>
+                        <p>{restaurant.address.building} {restaurant.address.street}, {restaurant.address.borough}, {restaurant.address.zipcode}</p>
+
+                        <div className='restaurant-text'>
+                            <p>Cuisine Type: {restaurant.cuisine}</p>
+                            <p>Description: {restaurant.description}</p>
+                        </div>
+                        
                         <div className="image-gallery">
                             {otherMediaImgs.map((img, index) => (
                                 <img key={index} src={`data:image/png;base64,${img}`} alt={`Other Media ${index + 1}`} className="gallery-image"/>
                             ))}
                         </div>
+                        <div className='restaurant-links'>
+                        </div>
+
                     </div>
                 ) : (
                     <div>
