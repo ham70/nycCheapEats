3bff26b5a1d6ff6a60042b72c0f705832b9f871b
Matthew Ham <hammatthew70@gmail.com>
2024-07-12 17:05:07 -0400

    add comments for how to use the baseurl in prod and dev branches for ease of use

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -1,10 +1,10 @@
 import axios from 'axios'
 
-//baseurl prod setup: 'https://nyccheapeats.onrender.com/restaurants' || http://localhost:5555/restaurants'
+//baseurl prod setup: 'https://nyccheapeats.onrender.com/restaurants' || 'http://localhost:5555/restaurants'
 //baseurl dev setup: 'http://localhost:5555/restaurants' || 'https://nyccheapeats.onrender.com/restaurants'
 
 export default axios.create({
-    baseURL: 'http://localhost:5555/restaurants' || 'https://nyccheapeats.onrender.com/restaurants',
+    baseURL:'https://nyccheapeats.onrender.com/restaurants' || 'http://localhost:5555/restaurants',
     headers: {
         'Content-Type': 'application/json'
     }
