752178d2e697aa88b84555f02c205cdc92d9e3e2
Matthew Ham <hammatthew70@gmail.com>
2024-09-10 23:44:13 -0400

    idk but i'm running out of ideas

--- backend/index.js
+++ backend/index.js
@@ -1,55 +1,55 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import mongoose from 'mongoose'
-import cors from 'cors'
-import restaurantsRoute from './routes/restaurantRoute.js'
-import restaurantUploadPagesRoute from './routes/restaurantUploadPagesRoute.js'
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import restaurantsRoute from './routes/restaurantRoute.js';
+import restaurantUploadPagesRoute from './routes/restaurantUploadPagesRoute.js';
 
 //==================================================================================
 //getting resources from .env file
-dotenv.config()
-const port = 8000 || process.env.PORT
-const url = process.env.MONGODBURL
+dotenv.config();
+const port = process.env.PORT || 8000;
+const url = process.env.MONGODBURL;
 
-//inializing app and handeling middleware
+//inializing app and handling middleware
 const app = express();
-app.use(express.json())
-app.use(cors(
-    {
-        origin: "https://nyccheapeats.vercel.app",
-    methods: ["GET", "POST"],
-    credentials: true
-    }
-))
-app.set("view engine", "ejs");
+
+// CORS configuration
+app.use(cors({
+    origin: 'https://nyccheapeats.vercel.app',
+    methods: ['GET', 'POST'],
+    credentials: true,
+}));
+
+app.use(express.json());
+app.set('view engine', 'ejs');
 
 //routes==================================================================================
-app.use('/restaurants', restaurantsRoute)
+app.use('/restaurants', restaurantsRoute);
 
-//these routes are exlcuded in prod
-//app.use('/restaurantUpload', restaurantUploadPagesRoute)
+//these routes are excluded in prod
+//app.use('/restaurantUpload', restaurantUploadPagesRoute);
 
 app.get('/', (request, response) => {
-    console.log(request)
-    return response.status(450).send("hello we are up and running")
-})
-
+    console.log(request);
+    return response.status(450).send('hello we are up and running');
+});
 
 //==================================================================================
-//connecting to database and running the sever
+//connecting to database and running the server
 mongoose
     .connect(url)
     .then(() => {
-        console.log('App connected to database')
+        console.log('App connected to database');
 
-        //only run the sever if we can connect to the database
+        //only run the server if we can connect to the database
         app.listen(port, () => {
-            console.log(`app is listening on port ${port}`)
-        })
+            console.log(`app is listening on port ${port}`);
+        });
         
     })
     .catch((error) => {
-        console.log(error)
-    })
+        console.log(error);
+    });
 
-export default app
+export default app;
