8ecbc2b6653b840b01b5ff507cd1e6b7a11f356f
Matthew Ham <hammatthew70@gmail.com>
2025-05-23 10:00:40 -0400

    added new map component using google maps api

---
+++ frontend/src/components/map.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {APIProvider, Map, Marker} from '@vis.gl/react-google-maps';
+
+const containerStyle = {
+  width: '100%',
+  height: '400px',
+}
+
+const  map = (center) => {
+  console.log(center)
+    return (
+      <div className='map'>
+        <APIProvider apiKey={process.env.REACT_APP_MAPSAPIKEY}>
+          <div style={containerStyle}>
+          <Map
+              defaultZoom={15}
+              defaultCenter={center.center}
+              >
+                <Marker position={center.center}/>
+          </Map>
+          </div>
+        </APIProvider>
+        </div>
+    )
+}
+export default map
