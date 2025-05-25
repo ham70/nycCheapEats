68069ba9a2ceeb85f9df9089939fd9a63b0ec216
Matthew Ham <hammatthew70@gmail.com>
2025-05-24 21:40:14 -0400

    minor updates for dev purposes

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -1,10 +1,10 @@
 import axios from 'axios'
 
-//baseurl prod setup: 'https://nyccheapeats.onrender.com/restaurants' || 'http://localhost:5555/restaurants'
-//baseurl dev setup: 'http://localhost:5555/restaurants' || 'https://nyccheapeats.onrender.com/restaurants'
+//baseurl prod setup: 'https://nyc-cheap-eats-server.vercel.app/restaurants' || 'http://localhost:8000/restaurants'
+//baseurl dev setup: 'http://localhost:8000/restaurants' || 'https://nyc-cheap-eats-server.vercel.app/restaurants'
 
 export default axios.create({
-    baseURL: 'https://nyc-cheap-eats-server.vercel.app/restaurants' || 'http://localhost:8000/restaurants',
+    baseURL:  'https://nyc-cheap-eats-server.vercel.app/restaurants' || 'http://localhost:8000/restaurants',
     headers: {
         'Content-Type': 'application/json'
     },
